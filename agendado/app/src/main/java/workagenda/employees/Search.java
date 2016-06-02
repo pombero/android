220f40cac4a2ee0bd7f77f70ee17ddf2c84b6c6e
Pombero <ricardobarbieri@gmail.com>
2016-06-02 00:06:37 -0300

    Add existing file

---
+++ agendado/app/src/main/java/workagenda/employees/Search.java
@@ -0,0 +1,14 @@
+package workagenda.employees;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Search extends Activity {
+    /** LLamado cuando la actividad es creada por primera vez. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
